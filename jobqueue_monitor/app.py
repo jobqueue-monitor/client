1cb1b138bbc512d107e27c1f1d35a0b164eb62d2
Justus Magin <keewis@posteo.de>
2024-04-24 14:12:51 +0200

    define title and subtitle and display in the app

--- jobqueue_monitor/app.py
+++ jobqueue_monitor/app.py
@@ -1,5 +1,10 @@
-from textual.app import App
+from textual.app import App, ComposeResult
+from textual.widgets import Header
 
 
 class JobqueueMonitor(App):
-    pass
+    TITLE = "jobqueue-monitor"
+    SUBTITLE = "monitor the status of a jobqueue"
+
+    def compose(self) -> ComposeResult:
+        yield Header()
