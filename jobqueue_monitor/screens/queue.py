65fb37f71ac8a79824d9c1b689f9b730d832ed82
Justus Magin <keewis@posteo.de>
2025-10-11 18:19:59 +0200

    render the queue resources

--- jobqueue_monitor/screens/queue.py
+++ jobqueue_monitor/screens/queue.py
@@ -71,93 +71,120 @@ class QueueScreen(Screen):
         self.app.push_screen(QueueDetailScreen(id=id, data=self.data[id]))
 
 
-class QueueInfo(Vertical):
-    def __init__(self, data):
-        label = Static("[i]Queue Info[/i]", id="queue_info_heading", classes="heading")
-        settings = GeneralQueueInfo(data)
-        permissions = PermissionsInfo(data)
+def render_permissions_table(data, **kwargs):
+    def ensure_list(value):
+        if value is None:
+            return []
+        elif isinstance(value, list):
+            return value
+        else:
+            return [value]
 
-        super().__init__(
-            label,
-            Horizontal(settings, permissions),
-            id="queue_info",
-            classes="queue_detail_container",
-        )
+    # keys:
+    # - "acl_user_enable"
+    # - "acl_users"
+    users = ensure_list(data.get("acl_users"))
+    enforced = bool(data.get("acl_user_enable", False))
+
+    table = DataTable(
+        name="Permissions",
+        zebra_stripes=True,
+        id="queue_permissions_table",
+        cursor_type="none",
+    )
+    table.add_columns("name", "value")
+    rows = [
+        ("enforced", enforced),
+        ("users", ", ".join(users)),
+    ]
+    table.add_rows(rows)
+    return table
 
 
-class PermissionsInfo(Vertical):
-    def __init__(self, data):
-        def ensure_list(value):
-            if value is None:
-                return []
-            elif isinstance(value, list):
-                return value
-            else:
-                return [value]
+def render_settings_table(data, **kwargs):
+    queue_info_keys = [
+        "queue_type",
+        "enabled",
+        "started",
+        "priority",
+    ]
+    table = DataTable(name="Settings", **kwargs)
+    table.add_columns("name", "value")
+    table.add_rows([(k, data.get(k, "(unset)")) for k in queue_info_keys])
+    return table
 
-        # keys:
-        # - "acl_user_enable"
-        # - "acl_users"
-        users = ensure_list(data.get("acl_users"))
-        enforced = bool(data.get("acl_user_enable", False))
 
-        label = Static(
-            "[i]Permissions[/i]", id="queue_permissions_heading", classes="heading"
-        )
-        table = DataTable(
-            name="Permissions",
-            zebra_stripes=True,
-            id="queue_permissions_table",
-            cursor_type="none",
-        )
-        table.add_columns("name", "value")
+def preprocess_resource_table(data):
+    translations = {
+        "memory": "mem",
+        "# MPI processes": "mpiprocs",
+        "# cpus": "ncpus",
+        "# nodes": "nodect",
+        "select": "select",
+        "walltime": "walltime",
+        "place": "place",
+    }
+
+    return {
+        key: data.get(data_key, "(unset)") for key, data_key in translations.items()
+    }
+
+
+def render_resource_table(data, **kwargs):
+    if data:
+        translated = preprocess_resource_table(data)
+        rows = list(translated.items())
+        disabled = False
+    else:
+        rows = []
+        disabled = True
+
+    table = DataTable(disabled=disabled, **kwargs)
+    table.add_columns("resource", "value")
+    table.add_rows(rows)
+
+    return table
+
+
+def render_resource_limits(data, **kwargs):
+    def preprocess_group_key(key):
+        return key.removeprefix("resources").lstrip("_")
+
+    expected_keys = [
+        "mem",
+        "ncpus",
+        "nodect",
+        "walltime",
+        "mpiprocs",
+        "place",
+        "select",
+    ]
+    resource_limits = {
+        key: {
+            preprocess_group_key(group): data.get(group, {}).get(key, "(unset)")
+            for group in ["resources_min", "resources_max", "resources_default"]
+        }
+        for key in expected_keys
+    }
+    translated = preprocess_resource_table(resource_limits)
+
+    if any(
+        value != "(unset)" for group in translated.values() for value in group.values()
+    ):
         rows = [
-            ("enforced", enforced),
-            ("users", ", ".join(users)),
+            (resource,) + tuple(group.values())
+            for resource, group in translated.items()
         ]
-        table.add_rows(rows)
+        disabled = False
+    else:
+        rows = []
+        disabled = True
 
-        super().__init__(
-            label,
-            table,
-            name="Permissions",
-            id="queue_permissions",
-            classes="queue_details",
-        )
+    table = DataTable(disabled=disabled, **kwargs)
+    table.add_columns("resource", "min", "max", "default")
+    table.add_rows(rows)
 
-
-class GeneralQueueInfo(Vertical):
-    def __init__(self, data):
-        queue_info_keys = [
-            "queue_type",
-            "enabled",
-            "started",
-            "priority",
-        ]
-
-        label = Static(
-            "[i]Settings[/i]", id="queue_settings_heading", classes="heading"
-        )
-        table = DataTable(
-            name="Settings",
-            zebra_stripes=True,
-            id="queue_settings_table",
-            cursor_type="none",
-        )
-        table.add_columns("name", "value")
-        table.add_rows([(k, data.get(k, "(unset)")) for k in queue_info_keys])
-
-        super().__init__(
-            label,
-            table,
-            name="Settings",
-            id="queue_general_info",
-            classes="queue_details",
-        )
-
-
-def render_resource_info(data):
-    return Static("resource info", classes="queue_detail_container")
+    return table
 
 
 def parse_state_count(string):
@@ -211,7 +238,76 @@ class QueueDetailScreen(ModalScreen):
         yield Static(
             f"[b]Queue: {self._queue_id}[/b]", id="queue_name", classes="queue_details"
         )
-        yield QueueInfo(self._data)
-        yield render_resource_info(self._data)
+
+        # queue info
+        with Vertical(id="queue_info", classes="queue_detail_container"):
+            yield Static(
+                "[i]Queue Info[/i]", id="queue_info_heading", classes="heading"
+            )
+
+            with Horizontal(id="queue_info_container"):
+                with Vertical(id="queue_settings", classes="queue_details"):
+                    yield Static(
+                        "[i]Settings[/i]",
+                        id="queue_settings_heading",
+                        classes="heading",
+                    )
+
+                    yield render_settings_table(
+                        self._data,
+                        zebra_stripes=True,
+                        id="queue_settings_table",
+                        cursor_type="none",
+                    )
+                with Vertical(id="queue_permissions", classes="queue_details"):
+                    yield Static(
+                        "[i]Permissions[/i]",
+                        id="queue_permissions_heading",
+                        classes="heading",
+                    )
+
+                    yield render_permissions_table(
+                        self._data,
+                        zebra_stripes=True,
+                        id="queue_permissions_table",
+                        cursor_type="none",
+                    )
+
+        with Vertical(id="resource_info", classes="queue_detail_container"):
+            yield Static(
+                "[i]Resources[/i]", id="queue_resource_heading", classes="heading"
+            )
+
+            with Horizontal(id="resources"):
+                with Vertical(id="assigned_resources", classes="queue_details"):
+                    yield Static(
+                        "[i]Assigned resources[/i]",
+                        id="queue_resource_available_heading",
+                        classes="heading",
+                    )
+
+                    yield render_resource_table(
+                        self._data.get("resources_assigned", {}),
+                        name="assigned_resources",
+                        zebra_stripes=True,
+                        id="queue_resource_assigned_table",
+                        cursor_type="none",
+                    )
+
+                with Vertical(id="resource_limits", classes="queue_details"):
+                    yield Static(
+                        "[i]Resource limits[/i]",
+                        id="queue_resource_limits_heading",
+                        classes="heading",
+                    )
+
+                    yield render_resource_limits(
+                        self._data,
+                        name="resource_limits",
+                        zebra_stripes=True,
+                        id="queue_resource_assigned_table",
+                        cursor_type="none",
+                    )
+
         yield render_job_summary(self._data)
         yield Footer()
