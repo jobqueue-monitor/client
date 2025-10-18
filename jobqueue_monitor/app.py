db3b57ba2a7a6238622baa1612276c2f7c932a06
Justus Magin <keewis@posteo.de>
2025-10-18 14:42:52 +0200

    make the app connect to the server using ssh

--- jobqueue_monitor/app.py
+++ jobqueue_monitor/app.py
@@ -1,10 +1,27 @@
+import asyncio
 from dataclasses import dataclass
 
+import asyncssh
+from textual import on, work
 from textual.app import App, ComposeResult
 from textual.binding import Binding
+from textual.message import Message
 from textual.widgets import Footer, Header
 
 from .screens import QueueDetailScreen, QueueScreen
+
+
+class SSHConnected(Message):
+    def __init__(self, connection):
+        super().__init__()
+        self.connection = connection
+
+
+async def search_executable(con, executable):
+    command = f"bash -c 'which {executable}'"
+    result = await con.run(command)
+
+    return result
 
 
 @dataclass
@@ -30,6 +47,43 @@ class JobqueueMonitor(App):
 
         super().__init__()
 
+        self._connection = None
+
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
+
+    @work(exclusive=True, group="connect", description="connecting to ssh server")
+    async def _connect(self) -> None:
+        connection = await asyncssh.connect(self.config.server)
+
+        self.post_message(SSHConnected(connection=connection))
+
+    @on(SSHConnected)
+    def launch_server(self, message: SSHConnected) -> None:
+        self._connection = message.connection
+
+        self.server_worker = self._launch_server()
+
+    @work(exclusive=True, group="launch-server", description="launch the server")
+    async def _launch_server(self) -> None:
+        server_executable = search_executable(
+            self._connection,
+            "jobqueue-monitor-server",
+        )
+        command = f"{server_executable} -p {self.config.remote_port}"
+        async with self._connection.create_process(command) as proc:
+            listener = await self._connection.forward_local_port(
+                "localhost",
+                self.config.local_port,
+                "localhost",
+                self.config.remote_port,
+            )
+            # run indefinitely
+            await asyncio.gather(proc.wait(), listener.wait_closed())
+
+    def on_mount(self):
+        self._connect()
+
+    def on_quit(self):
+        self._connection.close()
